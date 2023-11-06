2bf1363e4d044900b8ef500e69ad46cd883acdf4
Emmanuel <dsk.sasu@mercedes-benz.com>
2023-11-06 13:06:01 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [input, setInput] = useState<string>("");
+  const [result, setResult] = useState<string | null>(null);
+
+  const clearInput = () => {
+    setInput("");
+    setResult(null);
+  };
+
+  const calculateResult = () => {
+    try {
+      setResult(eval(input));
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const handleOperator = (operator: string) => {
+    setInput((prevInput) => prevInput + operator);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="input-section">
+        <div className="display">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <div className="operators">
+          <button onClick={() => handleOperator("+")}>+</button>
+          <button onClick={() => handleOperator("-")}>-</button>
+          <button onClick={() => handleOperator("*")}>*</button>
+          <button onClick={() => handleOperator("/")}>/</button>
+        </div>
+        <div className="buttons">
+          <button style={{ marginRight: "10px" }} onClick={clearInput}>
+            Clear
+          </button>
+          <button onClick={calculateResult}>Calculate</button>
+        </div>
+      </div>
+      <div className="result">{result !== null ? `Result: ${result}` : ""}</div>
     </div>
   );
-}
+};
 
 export default App;
